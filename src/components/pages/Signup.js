67a57ef438b49fc1686bd4a87640aa7111450403
asifhjahid <dhrubo8211diu@gmail.com>
2021-10-06 19:22:54 +0600

    signup and login auth

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import classes from '../../styles/Signup.module.css'
 import Illustration from '../Illustration'
-import Form from '../Form'
-import TextInput from '../TextInput'
-import Checkbox from '../Checkbox'
-import Button from '../Button'
+
 import signupImage from '../../assets/images/signup.svg'
+import SignupForm from '../SignupForm'
 
 export default function Signup() {
     return (
@@ -14,37 +11,7 @@ export default function Signup() {
 
             <div className='column'>
             <Illustration signupImage={signupImage} />
-            <Form className={`${classes.signup}`}>
-                <TextInput 
-                type='text'
-                placeholder='Enter Name'
-                icon='person'
-                />
-                <TextInput 
-                type='text'
-                placeholder='Enter email'
-                icon='alternate_email'
-                />
-                <TextInput 
-                type='password'
-                placeholder='Enter Password'
-                icon='lock'
-                />
-                <TextInput 
-                type='password'
-                placeholder='Confirm password'
-                icon='lock_clock'
-                />
-                <Checkbox 
-                type='checkbox'
-                text=' I agree to the Terms &amp; Conditions'/>
-                <Button>
-                    <span>Submit Now</span>
-                </Button>
-                <div className="info">
-                    Already have an account? <a href="login.html">Login</a> instead.
-                </div>
-            </Form>
+            <SignupForm />
             </div>
         </div>
     )
